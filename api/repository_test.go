8f516110ac890d2acd135c9e301d396f47d680e7
Aidas Degutis <aidas.degutis@interactio.io>
2022-04-20 17:19:53 +0300

    TestGetArticles moved to repositoryTestSuite

--- api/repository_test.go
+++ api/repository_test.go
@@ -1,35 +1,51 @@
 package api
 
 import (
-	assert "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"on951/database"
 	dbStructure "on951/database/structure"
 	"strconv"
 	"testing"
 )
 
-func TestGetArticles(t *testing.T) {
-	mAssert := assert.New(t)
+type repositoryTestSuite struct {
+	suite.Suite
+	db    database.TDatabaseMock
+	aRepo TArticlesRepository
+}
+
+func (suite *repositoryTestSuite) SetupSuite() {
+
+	suite.db.On("ConnectToDB", "dummy").Return(true)
+	suite.Assert().True(suite.db.ConnectToDB("dummy"), "Connecting to in-memory DB")
+	suite.db.AutoMigrate()
+	suite.aRepo = TArticlesRepository{IDatabase: &suite.db}
+
+}
+func (suite *repositoryTestSuite) TearDownSuite() {
+
+	suite.db.DisconnectDB()
+	suite.Nil(suite.db.GetDB(), "Disconnected in-memory db")
+}
+
+func TestRepository(t *testing.T) {
+	suite.Run(t, new(repositoryTestSuite))
+}
+
+func (suite *repositoryTestSuite) TestGetArticles() {
 
 	for _, testCase := range testGetArticlesData {
 
-		m := new(database.TDatabaseMock)
-
-		m.On("ConnectToDB", "dummy").Return(true)
-		assert.True(t, m.ConnectToDB("dummy"), "Connecting to in-memory DB")
-		m.AutoMigrate()
-
 		for i := 1; i <= testCase.articlesInDb; i++ {
 			record := dbStructure.ArticleBriefInfo{Title: "article " + strconv.Itoa(i)}
-			m.GetDB().Table(dbStructure.TableArticles).Create(&record)
+			suite.db.GetDB().Table(dbStructure.TableArticles).Create(&record)
 		}
 
-		aRepo := TArticlesRepository{
-			IDatabase: m,
-		}
+		articles := suite.aRepo.GetArticles(testCase.page, testCase.pageSize)
+		suite.Equal(len(articles), testCase.articlesCount)
+		suite.db.GetDB().Unscoped().AllowGlobalUpdate = true
+		tx := suite.db.GetDB().Debug().Unscoped().Delete(&dbStructure.Article{})
+		suite.Equal(int64(testCase.articlesInDb), tx.RowsAffected, "Deleted test articles")
 
-		articles := aRepo.GetArticles(testCase.page, testCase.pageSize)
-		mAssert.Equal(len(articles), testCase.articlesCount)
-		m.DisconnectDB()
 	}
 }
