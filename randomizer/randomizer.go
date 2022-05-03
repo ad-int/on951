b9d77229285811f6cf6d386a69d94897c2053d37
Aidas Degutis <aidas.degutis@interactio.io>
2022-05-03 09:55:08 +0300

    move randomizer tests to test suite + random image generator

--- randomizer/randomizer.go
+++ randomizer/randomizer.go
@@ -1,6 +1,13 @@
 package randomizer
 
 import (
+	"bytes"
+	"encoding/base64"
+	"errors"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"math/rand"
 	"strings"
 	"time"
@@ -8,12 +15,12 @@ import (
 
 var characters = "abcdefghjklmnoprstuvwxyz0123456789`!@#$%^&*()_-+="
 
-func GetRandomString(length int, size int) string  {
-var charactersSlice []string
+func GetRandomString(length int, size int) string {
+	var charactersSlice []string
 
 	rand.Seed(time.Now().UnixNano())
 
-	charactersSlice = strings.Split(characters,"")
+	charactersSlice = strings.Split(characters, "")
 
 	var randomStr []string
 	var element string
@@ -26,4 +33,29 @@ var charactersSlice []string
 
 	}
 	return strings.Join(randomStr, "")
+}
+
+func randomBase64Image(mimeType string, width int, height int) (string, string, error) {
+	var err error
+	rand.Seed(time.Now().UnixNano())
+	r := image.Rectangle{Min: image.Pt(0, 0), Max: image.Pt(width, height)}
+	img := image.NewNRGBA(r)
+	buf := bytes.NewBuffer([]byte{})
+	switch mimeType {
+	case "image/png":
+		err = png.Encode(buf, img)
+		break
+	case "image/jpeg":
+		err = jpeg.Encode(buf, img, &jpeg.Options{Quality: 100})
+		break
+	case "image/gif":
+		err = gif.Encode(buf, img, &gif.Options{NumColors: 256})
+		break
+	default:
+		err = errors.New("unknown image mime type")
+		mimeType = ""
+		break
+	}
+
+	return mimeType, base64.StdEncoding.EncodeToString(buf.Bytes()), err
 }
--- randomizer/randomizer_test.go
+++ randomizer/randomizer_test.go
@@ -1,13 +1,75 @@
 package randomizer
 
 import (
-	"github.com/stretchr/testify/assert"
+	"encoding/base64"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-func TestGetRandomString(t *testing.T) {
+type TestCaseType struct {
+	mimeType      string
+	width         int
+	height        int
+	triggersError bool
+}
+type randomizerTestSuite struct {
+	suite.Suite
+	testCases []TestCaseType
+}
 
-	assert.Len(t, GetRandomString(10, 10), 100)
-	assert.Len(t, GetRandomString(10, 1), 10)
-	assert.Len(t, GetRandomString(5, 2), 10)
+func (suite *randomizerTestSuite) SetupSuite() {
+	suite.testCases = []TestCaseType{
+		{
+			mimeType:      "image/png",
+			width:         0,
+			height:        0,
+			triggersError: true,
+		},
+		{
+			mimeType:      "image/jpeg",
+			width:         100,
+			height:        100,
+			triggersError: false,
+		},
+		{
+			mimeType:      "image/jpg",
+			width:         100,
+			height:        100,
+			triggersError: true,
+		},
+		{
+			mimeType:      "image/gif",
+			width:         55,
+			height:        20,
+			triggersError: false,
+		},
+	}
+}
+
+func TestRandomizerTestSuite(t *testing.T) {
+	suite.Run(t, new(randomizerTestSuite))
+}
+
+func (suite *randomizerTestSuite) TestGetRandomString() {
+
+	suite.Len(GetRandomString(10, 10), 100)
+	suite.Len(GetRandomString(10, 1), 10)
+	suite.Len(GetRandomString(5, 2), 10)
+}
+
+func (suite *randomizerTestSuite) TestGetBase64Image() {
+	var mimeType, img string
+	var err error
+	for _, testCase := range suite.testCases {
+		mimeType, img, err = randomBase64Image(testCase.mimeType, testCase.width, testCase.height)
+		suite.T().Log("generated image", mimeType, img)
+		if testCase.triggersError {
+			suite.Empty(img, "encoded image")
+			suite.NotNil(err)
+		} else {
+			decodedImg, _ := base64.StdEncoding.DecodeString(img)
+			suite.NotEmpty(mimeType, "mime type")
+			suite.NotEmpty(decodedImg, "decoded image")
+		}
+	}
 }
